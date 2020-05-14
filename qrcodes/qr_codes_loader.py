65fc695b43daef36fc5f48f8821c5bd05504d2ed
Leonardo Blanger <leonardoblanger@gmail.com>
2020-05-14 09:39:53 -0300

    Refined the data loading to read and resize the images.

--- qrcodes/qr_codes_loader.py
+++ qrcodes/qr_codes_loader.py
@@ -1,11 +1,28 @@
-import numpy as np
 from os import path
 import pandas as pd
 import tensorflow as tf
 
 
-def load_qr_codes_dataset(root=path.join("data", "qr_codes"),
-                          split='train'):
+def load_image(image_path, boxes, new_size):
+    image = tf.io.read_file(image_path)
+    image = tf.image.decode_jpeg(image)
+    prior_shape = tf.shape(image)
+
+    xmin = boxes[:, 0] * new_size[1] / prior_shape[1]
+    ymin = boxes[:, 1] * new_size[0] / prior_shape[0]
+    xmax = boxes[:, 2] * new_size[1] / prior_shape[1]
+    ymax = boxes[:, 3] * new_size[0] / prior_shape[0]
+
+    boxes = tf.stack([xmin, ymin, xmax, ymax], axis=-1)
+    image = tf.image.resize(image, new_size)
+    image = tf.cast(image, dtype=tf.uint8)
+
+    return image, boxes
+
+
+def load_qr_codes_dataset(root=path.join("..", "data", "qr_codes"),
+                          split='train',
+                          image_size=None):
     annotations_file = path.join(root, "qr_codes_{}.csv".format(split))
     annotations = pd.read_csv(annotations_file,
                               dtype={'image_id': str})
@@ -32,4 +49,9 @@ def load_qr_codes_dataset(root=path.join("data", "qr_codes"),
     ymaxs = tf.ragged.constant(ymaxs)
     bnd_boxes = tf.stack([xmins, ymins, xmaxs, ymaxs], axis=2)
 
-    return tf.data.Dataset.from_tensor_slices((image_paths, bnd_boxes))
+    dataset = tf.data.Dataset.from_tensor_slices((image_paths, bnd_boxes))
+
+    if image_size is not None:
+        dataset = dataset.map(lambda x, y: load_image(x, y, image_size))
+
+    return dataset
