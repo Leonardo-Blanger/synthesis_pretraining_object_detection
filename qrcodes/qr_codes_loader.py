adcaa049305aaa23fb9edb056ee4f72b07a5de83
Leonardo Blanger <leonardoblanger@gmail.com>
2020-05-17 13:44:43 -0300

    Split the image loading and resizing from the rest of the dataset loading.

--- qrcodes/qr_codes_loader.py
+++ qrcodes/qr_codes_loader.py
@@ -3,26 +3,30 @@ import pandas as pd
 import tensorflow as tf
 
 
-def load_image(image_path, boxes, new_size):
+def read_and_resize_image(image_path, boxes, new_size):
     image = tf.io.read_file(image_path)
     image = tf.image.decode_jpeg(image)
-    prior_shape = tf.shape(image)
 
-    xmin = boxes[:, 0] * new_size[1] / prior_shape[1]
-    ymin = boxes[:, 1] * new_size[0] / prior_shape[0]
-    xmax = boxes[:, 2] * new_size[1] / prior_shape[1]
-    ymax = boxes[:, 3] * new_size[0] / prior_shape[0]
+    if new_size is not None:
+        prior_shape = tf.shape(image)
 
-    boxes = tf.stack([xmin, ymin, xmax, ymax], axis=-1)
-    image = tf.image.resize(image, new_size)
-    image = tf.cast(image, dtype=tf.uint8)
+        xmin = boxes[:, 0] * new_size[1] / prior_shape[1]
+        ymin = boxes[:, 1] * new_size[0] / prior_shape[0]
+        xmax = boxes[:, 2] * new_size[1] / prior_shape[1]
+        ymax = boxes[:, 3] * new_size[0] / prior_shape[0]
+        labels = tf.cast(boxes[:, 4], xmin.dtype)
+
+        boxes = tf.stack([xmin, ymin, xmax, ymax, labels], axis=-1)
+        image = tf.image.resize(image, new_size)
+        image = tf.cast(image, dtype=tf.uint8)
+        boxes = tf.cast(boxes, dtype=tf.float32)
+        boxes = tf.RaggedTensor.from_tensor(boxes)
 
     return image, boxes
 
 
 def load_qr_codes_dataset(root=path.join("..", "data", "qr_codes"),
-                          split='train',
-                          image_size=None):
+                          split='train'):
     annotations_file = path.join(root, "qr_codes_{}.csv".format(split))
     annotations = pd.read_csv(annotations_file,
                               dtype={'image_id': str})
@@ -43,15 +47,11 @@ def load_qr_codes_dataset(root=path.join("..", "data", "qr_codes"),
         xmaxs.append(xmax)
         ymaxs.append(ymax)
 
-    xmins = tf.ragged.constant(xmins)
-    ymins = tf.ragged.constant(ymins)
-    xmaxs = tf.ragged.constant(xmaxs)
-    ymaxs = tf.ragged.constant(ymaxs)
-    bnd_boxes = tf.stack([xmins, ymins, xmaxs, ymaxs], axis=2)
+    xmins = tf.ragged.constant(xmins, dtype='int32')
+    ymins = tf.ragged.constant(ymins, dtype='int32')
+    xmaxs = tf.ragged.constant(xmaxs, dtype='int32')
+    ymaxs = tf.ragged.constant(ymaxs, dtype='int32')
+    labels = tf.ones_like(xmins, dtype='int32')
+    bnd_boxes = tf.stack([xmins, ymins, xmaxs, ymaxs, labels], axis=2)
 
-    dataset = tf.data.Dataset.from_tensor_slices((image_paths, bnd_boxes))
-
-    if image_size is not None:
-        dataset = dataset.map(lambda x, y: load_image(x, y, image_size))
-
-    return dataset
+    return tf.data.Dataset.from_tensor_slices((image_paths, bnd_boxes))
